a716aea6ccec2a655c104334004312122610db30
Gambar <gambarli.gambar@gmail.com>
2022-06-02 14:58:11 +0400

    Add project files.

---
+++ FirstWPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_LayoutUpdated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                Random r = new Random();
+                Brush brush = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 233)));
+                btn.Background = brush;
+                MessageBox.Show($"Name: {btn.Name}\nColor: {btn.Background}");
+            }
+        }
+
+        private void Button_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                if (panel_1.Children.Contains(btn))
+                    panel_1.Children.Remove(btn);
+                else
+                    panel_2.Children.Remove(btn);
+                
+                Title = (string)btn.Content;
+            }
+        }
+
+       
+
+    }
+}
